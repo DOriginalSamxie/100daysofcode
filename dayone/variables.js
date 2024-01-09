6cb84738aba45596cf7153a89849030c9bf5906b
DOriginalSamxie <samadrufai@gmail.com>
2024-01-09 20:18:15 +0100

    day one

---
+++ dayone/variables.js
@@ -0,0 +1,18 @@
+// Declaring variables
+let firstName = 'Samad'
+let age = 419
+let isDev = false
+
+// Displayng value (CONCATINATION)
+console.log('Name: ' + firstName)
+console.log('Age: ' + age)
+console.log('Is a Developer: ' + isDev)
+
+//Dara Types
+let car = 'sudan' //String
+let mileDriven = 5000 //Number
+let isTesla = false //Boolean
+let features = ['GPS', 'Bluetooth', 'Backup Camera'] //Array
+
+// Displaying values and data types woth conctination exercises
+console.log('Car: ' + car + ' (Type: ' + typeof car + ')')
